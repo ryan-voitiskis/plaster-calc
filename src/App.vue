bfbc4035f818ecaec64224abed2d982415f7bebd
ryan-voitiskis <ryanvoitiskis@pm.me>
2023-01-15 01:45:47 +1100

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Plaster calc</h1>
+    <nav>
+      <router-link to="/">Products to use</router-link>
+      <router-link to="/">Areas to sheet</router-link>
+      <router-link to="/">Material list</router-link>
+    </nav>
+  </header>
+  <main>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
